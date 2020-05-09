6ebdc39e555a78a3b35105788eb913cbaab5a46f
Peter <peterdotliu@gmail.com>
2020-05-09 01:41:40 -0400

    Create commands directory, Implement dynamic command execution

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
+require('dotenv').config()
+const fs = require('fs')
 const Discord = require('discord.js')
 const { prefix } = require('./config.json')
 const client = new Discord.Client()
-require('dotenv').config()
+client.commands = new Discord.Collection()
+
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+
+  client.commands.set(command.name, command)
+}
 
 client.once('ready', () => {
   console.log('Ready!')
 })
 
 client.on('message', msg => {
-  if (!msg.content.startsWith(prefix) || msg.author.bot) return
-
   const args = msg.content.slice(prefix.length).split(/ +/)
   const command = args.shift().toLowerCase()
 
-  if (command === 'sunny') {
-    msg.channel.send(`@${msg.author.username} hrrrro burddy`)
+  if (!client.commands.has(command)) return
+
+  try {
+    client.commands.get(command).execute(msg, args)
+  } catch (err) {
+    console.error(err)
+    msg.reply('Error with command')
   }
 })
 
