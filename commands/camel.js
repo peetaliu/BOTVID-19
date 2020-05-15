73db180e1947a42e2b389f83ea0ea8472b860c56
Peter <peterdotliu@gmail.com>
2020-05-14 23:21:03 -0400

    Finish camel command

--- commands/camel.js
+++ commands/camel.js
@@ -1,9 +1,33 @@
 module.exports = {
   name: 'camel',
   execute(msg, args) {
-    const msgArr = msg.content.split('')
-    console.log(msgArr)
-    const upCase = Math.floor(msgArr.length / 2)
-    console.log(msgArr.map(l => l.toString().toUpperCase()))
+    console.log(`Message: ${msg.content} Sent by: ${msg.author.id}`)
+    const convertString = str => {
+      let convStr = str.split('')
+      if (str.length === 1) {
+        return convStr
+      }
+      if (str.length < 4) {
+        const up = Math.floor(Math.random() * (str.length - 1) + 1)
+        convStr[up] = convStr[up].toUpperCase()
+        return convStr.join('')
+      }
+
+      let posUp = []
+      for (i = 0; i < str.length - 1; i++) {
+        posUp[i] = i + 1
+      }
+      console.log('initial posUp', posUp)
+      for (i = 0; i < Math.floor(str.length / 2); i++) {
+        const up = Math.floor(Math.random() * posUp.length)
+        convStr[posUp[up]] = convStr[posUp[up]].toUpperCase()
+        posUp.splice(up, 1)
+        console.log('posUp', posUp)
+      }
+      return convStr.join('')
+    }
+
+    const msgArr = msg.content.toLowerCase().split(' ')
+    msg.channel.send(msgArr.map(s => convertString(s)).join(' '))
   },
 }
